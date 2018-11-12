80a3e3e71a069b9d06bc9fce561c61a881830bf4
Pilar <pilarcanalsdros@gmail.com>
2018-11-12 18:15:35 +0100

    NAs &  PLOTS

--- POWER.R
+++ POWER.R
@@ -16,9 +16,10 @@ library(lubridate)
 #install.packages("tidyverse")
 library(tidyverse)
 
+
 #### A_SETTING FILES####
 
-setwd("C:/Users/pilar/Google Drive/A_DATA/UBIQUM/TASK3.1/git/task-3-1-define-a-data-science-process-PCANALS")
+setwd("C:/Users/pilar/Google Drive/A_DATA/UBIQUM/TASK3.1/task-3-1-define-a-data-science-process-PCANALS")
 
 powero<-read.csv(file = "household_power_consumption.txt", header = TRUE, sep =";", na.strings=c("NA", "-", "?"), stringsAsFactors=FALSE)
 
@@ -26,37 +27,52 @@ powero<-read.csv(file = "household_power_consumption.txt", header = TRUE, sep ="
 #"stringsAsFactors=FALSE" don't converts the variables in factors
 
 
-str(powero)
-dim(powero)
-summary(powero)
-apply(powero, 2, class)
+# str(powero)
+# dim(powero)
+# summary(powero)
+# apply(powero, 2, class)
 
-power<-powero
-
-power<-cbind(power,paste(power$Date,power$Time), stringsAsFactors=FALSE)
-colnames(power)[10] <-"DateTime"
-power <- power[,c(ncol(power), 1:(ncol(power)-1))]
-head(power)
+power<-powero # to keep origen#
 
 
+power$DateTime<-lubridate::dmy_hms(paste(power$Date,power$Time))
 power$Date<-lubridate::dmy(power$Date)
 power$Time<-lubridate::hms(power$Time)
-power$DateTime<-lubridate::dmy_hms(power$DateTime)### error#
-
-###warning error Warning messages:
-# 1: All formats failed to parse. No formats found. 
-# 2: In doTryCatch(return(expr), name, parentenv, handler) :
-#   restarting interrupted promise evaluation
-
-#'data.frame':	2075259 obs. of  10 variables:
-# $ DateTime             : POSIXct, format: "2006-12-16 17:24:00" "2006-12-16 17:25:00" "2006-12-16 17:26:00" "2006-12-16 17:27:00" ...
-# $ Date                 : Date, format: "2006-12-16" "2006-12-16" "2006-12-16" "2006-12-16" ...
-# $ Time                 :Formal class 'Period' [package "lubridate"] with 6 slots
 
 
-str(power)
+#each attribute it's different but it works correctly for time series#
+
+power <- power[,c(ncol(power), 1:(ncol(power)-1))] #order 
+
+
+power %>% head() %>% str()
+
+
+#power_na<-dplyr::filter(is.na(power))
+
+#power_na<-which(is.na(power))
+
+#power_na<-subset(is.na(power))
+
+power_na <- power[rowSums(is.na(power)) > 0,]
+
+#### PLOTS ####
+hist(power$Global_active_power, main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)", col = "mediumturquoise")
+
+plot(power$Global_active_power ~ power$DateTime, 
+     ylab = "Global Active Power (kilowatts)", xlab = "", type = "l")
+
+plot(power$Sub_metering_1 ~ power$DateTime, ylab = "Energy sub metering", xlab = "", type = "l")
+lines(power$Sub_metering_2 ~ power$DateTime, col = 'Red')
+lines(power$Sub_metering_3 ~ power$DateTime, col = 'Blue')
+legend("topright", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd = 1)
+
 
 ####~~~~NOTES - NEXT STEPS~~~~####
+
+
+#transform txt in a r file#
 
 #dyplr tratar NA, ver dataframe solo na#
 
